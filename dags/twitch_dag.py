0530e66312e2c1567fccc622adcea68df87ceaf0
BillieJoe <mauricio.arbelaiz@gmail.com>
2024-06-15 14:00:34 -0300

    ADD, apache - fail-safe - alerts

--- dags/twitch_dag.py
+++ dags/twitch_dag.py
@@ -1,18 +1,35 @@
 from datetime import datetime, timedelta
-from airflow import DAG # type: ignore
+from airflow import DAG  # type: ignore
 from airflow.operators.python_operator import PythonOperator # type: ignore
+from airflow.utils.email import send_email # type: ignore
 from twitch_etl import obtener_token, extraer_datos
 
 # Argumentos
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 1, 1),
-    'email_on_failure': False,
+    'start_date': datetime(2024, 1, 1), # feche por si es necesario hacer un backfill
+    'email_on_failure': True,  # envío de correo si falla
     'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retries': 3,  # 3 intentos 
+    'retry_delay': timedelta(minutes=5),  # retardo entre intentos
 }
+
+# enviar correo electronico alerta si falla
+def task_failure_alert(context):
+    """Send email alert on task failure."""
+    dag_id = context.get('dag').dag_id
+    task_id = context.get('task_instance').task_id
+    execution_date = context.get('execution_date')
+    log_url = context.get('task_instance').log_url
+    email_subject = f"Airflow alert: {dag_id}.{task_id} Failed"
+    email_body = f"""
+    DAG: {dag_id}<br>
+    Task: {task_id}<br>
+    Execution Time: {execution_date}<br>
+    Log: <a href="{log_url}">Link</a><br>
+    """
+    send_email('admin@admin.com', email_subject, email_body)
 
 # DAG
 dag = DAG(
@@ -27,14 +44,17 @@ get_token_task = PythonOperator(
     task_id='get_twitch_access_token',
     python_callable=obtener_token,
     dag=dag,
+    on_failure_callback=task_failure_alert,  # callback por si falla
+    retries=default_args['retries'],  # numero de intentos definido arriba
+    retry_delay=default_args['retry_delay'],  # retardo entre intentos definido arriba
 )
 
-# Eextraer los datos
+# Extraer los datos
 extract_task = PythonOperator(
     task_id='extract_twitch_data',
     python_callable=extraer_datos,
     dag=dag,
+    on_failure_callback=task_failure_alert,  # agrega callback por si falla
+    retries=default_args['retries'],  # numero de reintentos definido arriba
+    retry_delay=default_args['retry_delay'],  # retardo entre intentos definido arriba
 )
-
-# Establecer la dependencia entre las tareas
-get_token_task >> extract_task
