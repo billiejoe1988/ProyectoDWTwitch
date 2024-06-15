bf2f7b1622fdb48623b311655afc19375b9fc71b
BillieJoe <mauricio.arbelaiz@gmail.com>
2024-06-15 14:43:02 -0300

    ADD, sacarle el try

--- dags/twitch_etl.py
+++ dags/twitch_etl.py
@@ -23,137 +23,127 @@ twitch_config = {
 }
 
 def obtener_token():
-    try:
-        url = 'https://id.twitch.tv/oauth2/token'
-        params = {
-            'client_id': twitch_config['client_id'],
-            'client_secret': twitch_config['client_secret'],
-            'grant_type': 'client_credentials'
-        }
+    url = 'https://id.twitch.tv/oauth2/token'
+    params = {
+        'client_id': twitch_config['client_id'],
+        'client_secret': twitch_config['client_secret'],
+        'grant_type': 'client_credentials'
+    }
 
-        response = requests.post(url, data=params)
+    response = requests.post(url, data=params)
 
-        if response.status_code == 200:
-            return response.json().get('access_token')
-        else:
-            raise RuntimeError(f'Error al obtener el token de acceso: {response.status_code}')
-
-    except Exception as e:
-        print(f'Error en obtener_token: {str(e)}')
-        raise
+    if response.status_code == 200:
+        return response.json().get('access_token')
+    else:
+        print(f'Error al obtener el token de acceso: {response.status_code}')
+        return None
 
 def extraer_datos():
-    try:
-        access_token = obtener_token()
+    access_token = obtener_token()
 
-        if access_token:
-            url = 'https://api.twitch.tv/helix/streams'
-            params = {'first': 100}  
+    if access_token:
+        url = 'https://api.twitch.tv/helix/streams'
+        params = {'first': 100}  
 
-            headers = {
-                'Client-Id': twitch_config['client_id'],
-                'Authorization': f'Bearer {access_token}'
-            }
+        headers = {
+            'Client-Id': twitch_config['client_id'],
+            'Authorization': f'Bearer {access_token}'
+        }
 
-            response = requests.get(url, params=params, headers=headers)
+        response = requests.get(url, params=params, headers=headers)
 
-            if response.status_code == 200:
-                data = response.json()
+        if response.status_code == 200:
+            data = response.json()
 
-                # Guardar en JSON
-                with open('twitch_data.json', 'w') as json_file:
-                    json.dump(data, json_file)
+            # Guardar en JSON
+            with open('twitch_data.json', 'w') as json_file:
+                json.dump(data, json_file)
 
-                # Crear DataFrame a partir de los datos extraídos
-                streams = []
-                for stream in data['data']:
-                    streams.append({
-                        'stream_id': stream['id'],
-                        'user_id': stream['user_id'],
-                        'user_name': stream['user_name'],
-                        'game_id': stream['game_id'],
-                        'type': stream['type'],
-                        'title': stream['title'],
-                        'viewer_count': stream['viewer_count'],
-                        'started_at': stream['started_at'],
-                        'language': stream['language'],
-                        'thumbnail_url': stream.get('thumbnail_url', None),
-                        'channel_url': f"https://www.twitch.tv/{stream['user_name']}",
-                        'ingestion_time': datetime.now().isoformat(),
-                        'PRIMARY_KEY': f"{stream['id']}_{stream['user_id']}"
-                    })
+            # Crear DataFrame a partir de los datos extraídos
+            streams = []
+            for stream in data['data']:
+                streams.append({
+                    'stream_id': stream['id'],
+                    'user_id': stream['user_id'],
+                    'user_name': stream['user_name'],
+                    'game_id': stream['game_id'],
+                    'type': stream['type'],
+                    'title': stream['title'],
+                    'viewer_count': stream['viewer_count'],
+                    'started_at': stream['started_at'],
+                    'language': stream['language'],
+                    'thumbnail_url': stream.get('thumbnail_url', None),
+                    'channel_url': f"https://www.twitch.tv/{stream['user_name']}",
+                    'ingestion_time': datetime.now().isoformat(),
+                    'PRIMARY_KEY': f"{stream['id']}_{stream['user_id']}"
+                })
 
-                df = pd.DataFrame(streams)
+            df = pd.DataFrame(streams)
+            
+            # Verificar y eliminar filas duplicadas
+            df.drop_duplicates(inplace=True)
+            
+            # Verificar valores nulos y eliminar filas con valores nulos
+            df.dropna(inplace=True)
 
-                # Verificar y eliminar duplicadas
-                df.drop_duplicates(inplace=True)
+            # Definir límites superiores e inferiores para la columna 'viewer_count'
+            upper_limit = df['viewer_count'].quantile(0.99999)
+            lower_limit = df['viewer_count'].quantile(0.01)
 
-                # Verificar valores nulos y eliminar valores nulos
-                df.dropna(inplace=True)
+            # Filtrar valores atípicos fuera de los límites
+            df = df[(df['viewer_count'] >= lower_limit) & (df['viewer_count'] <= upper_limit)]
+            
+            # Exportar los datos a un  CSV
+            df.to_csv('twitch_data.csv', index=False)
 
-                # Definir límites superiores e inferiores para la columna 'viewer_count'
-                upper_limit = df['viewer_count'].quantile(0.99999)
-                lower_limit = df['viewer_count'].quantile(0.01)
+            # Conexión con Redshift
+            conn = psycopg2.connect(**redshift_config)
+            cur = conn.cursor()
 
-                # Filtrar valores fuera de los límites
-                df = df[(df['viewer_count'] >= lower_limit) & (df['viewer_count'] <= upper_limit)]
+            # Eliminar tabla si existe
+            cur.execute("DROP TABLE IF EXISTS twitch_streams;")
 
-                # Exportar los datos a un CSV
-                df.to_csv('twitch_data.csv', index=False)
+            # Crear tabla si no existe
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS twitch_streams (
+                stream_id VARCHAR(255),
+                user_id VARCHAR(255),
+                user_name VARCHAR(255),
+                game_id VARCHAR(255),
+                type VARCHAR(50),
+                title VARCHAR(500),
+                viewer_count INT,
+                started_at TIMESTAMP,
+                language VARCHAR(50),
+                thumbnail_url VARCHAR(500),
+                channel_url VARCHAR(500),
+                ingestion_time TIMESTAMP,
+                PRIMARY KEY (stream_id, user_id),
+                PRIMARY_KEY VARCHAR(255)
+            );
+            """)
 
-                # Conexión con Redshift
-                conn = psycopg2.connect(**redshift_config)
-                cur = conn.cursor()
-
-                # Eliminar tabla si existe
-                cur.execute("DROP TABLE IF EXISTS twitch_streams;")
-
-                # Crear tabla si no existe
+            # Insertar datos en la tabla
+            for _, row in df.iterrows():
                 cur.execute("""
-                CREATE TABLE IF NOT EXISTS twitch_streams (
-                    stream_id VARCHAR(255),
-                    user_id VARCHAR(255),
-                    user_name VARCHAR(255),
-                    game_id VARCHAR(255),
-                    type VARCHAR(50),
-                    title VARCHAR(500),
-                    viewer_count INT,
-                    started_at TIMESTAMP,
-                    language VARCHAR(50),
-                    thumbnail_url VARCHAR(500),
-                    channel_url VARCHAR(500),
-                    ingestion_time TIMESTAMP,
-                    PRIMARY KEY (stream_id, user_id),
-                    PRIMARY_KEY VARCHAR(255)
-                );
-                """)
+                INSERT INTO twitch_streams (stream_id, user_id, user_name, game_id, type, title, viewer_count, started_at, language, thumbnail_url, channel_url, ingestion_time, PRIMARY_KEY) 
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, 
+                (
+                    row['stream_id'], row['user_id'], row['user_name'], row['game_id'], row['type'], 
+                    row['title'], row['viewer_count'], row['started_at'], row['language'], 
+                    row['thumbnail_url'], row['channel_url'], row['ingestion_time'], row['PRIMARY_KEY']
+                ))
 
-                # Insertar datos en la tabla
-                for _, row in df.iterrows():
-                    cur.execute("""
-                    INSERT INTO twitch_streams (stream_id, user_id, user_name, game_id, type, title, viewer_count, started_at, language, thumbnail_url, channel_url, ingestion_time, PRIMARY_KEY) 
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                    """, 
-                    (
-                        row['stream_id'], row['user_id'], row['user_name'], row['game_id'], row['type'], 
-                        row['title'], row['viewer_count'], row['started_at'], row['language'], 
-                        row['thumbnail_url'], row['channel_url'], row['ingestion_time'], row['PRIMARY_KEY']
-                    ))
-
-                # Confirmar cambios, cerrar cursor y conexión
-                conn.commit()
-                cur.close()
-                conn.close()
-
-            else:
-                raise RuntimeError(f'Error al realizar la solicitud: {response.status_code}')
+            # Confirmar cambios, close cursor y conexion
+            conn.commit()
+            cur.close()
+            conn.close()
 
         else:
-            raise RuntimeError('No se pudo obtener el token de acceso.')
-
-    except Exception as e:
-        print(f'Error en extraer_datos: {str(e)}')
-        raise
+            print(f'Error al realizar la solicitud: {response.status_code}')
+    else:
+        print('No se pudo obtener el token de acceso.')
 
 if __name__ == "__main__":
     # Extraer y cargar datos en Redshift
@@ -165,5 +155,5 @@ if __name__ == "__main__":
     df = pd.read_sql(sql_query, conn)
     conn.close()
 
-    # Mostrar los primeros registros del DataFrame
+    # Mostrar los primeros registros del DataFrame para checkear si esta funcionando ok.
     print(df.head())
